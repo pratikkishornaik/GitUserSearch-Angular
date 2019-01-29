afb39d66d8706c50059e0c0dec0f6a8f8764926e
Pratik Naik <pratik.naik@coditas.com>
2019-01-29 12:35:02 +0530

    changed the file path

--- src/server.js
+++ src/server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/gitusersearch-angular-application'));
+app.use(express.static('./dist/gitSearch'));
 
 app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname, '/dist/your_app_name/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/gitSearch/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
